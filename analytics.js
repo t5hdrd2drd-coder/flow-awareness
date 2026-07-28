5c827948d5c3fdfbb1cd94c4eda205aa70989094
t5hdrd2drd-coder <t5hdrd2drd@privaterelay.appleid.com>
2026-07-28 21:49:41 +0800

    Revert "Install Vercel Web Analytics"

--- analytics.js
+++
@@ -1,8 +0,0 @@
-// Vercel Web Analytics
-import { inject } from './node_modules/@vercel/analytics/dist/index.mjs';
-
-// Initialize Vercel Analytics
-inject({
-  mode: 'auto', // Uses 'production' when deployed, 'development' locally
-  debug: false
-});
